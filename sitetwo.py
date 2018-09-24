332a7e69fae66193c0095b06b032edd6a2c9dc40
Paul Wilson <paul.wilson717@gmail.com>
2018-09-24 12:14:02 +0100

    Added some more stuff to the second site

--- sitetwo.py
+++ sitetwo.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,5 +17,21 @@ def goodbye():
 def general():
 	return "Genersl Kenobi"
 
+@app.route("/private")
+def private():
+	#assume a test for user log in failed
+	#so rdirect to log in user
+	return redirect(url_for('login'))
+
+@app.route("/login")
+def login():
+	return "Now we would get the username and password"
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return "You lost? We cant find this page",404
+
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
+
