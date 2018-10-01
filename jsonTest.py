1070991f3dd90784c7df33e41c5dde34fe71b48d
Paul Wilson <paul.wilson717@gmail.com>
2018-10-01 18:04:36 +0100

    some uncomplete testing

---
+++ jsonTest.py
@@ -0,0 +1,12 @@
+from flask import Flask, request 
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+	value = request.json['key']
+	return value
+
+if __name__ == '__main__':
+	app.run(debug=true)
